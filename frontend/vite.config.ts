61fad92aa1e340623a2f8b094424de955b6daf26
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2026-01-07 18:51:20 +0000

    Fix. issue js and css bundles not loading in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+  base: '/',
 });
