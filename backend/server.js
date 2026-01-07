4d2021e1a7290974fbdc6924b8129b673fcc52b0
Abhay Shrivastava <abhayshrivastava830@gmail.com>
2026-01-07 19:08:43 +0000

    Fix. env variables not copied in the docker image

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const port = PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ['http://3.137.136.122:31000', 'http://localhost:5173', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(compression());
 
